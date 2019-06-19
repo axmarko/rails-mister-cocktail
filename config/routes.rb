264b438b7928f812d659200930afad4a3e007d3b
Axel Marko <axkmar@gmail.com>
2019-06-19 12:40:32 +0300

    Finished with cocktails

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  get 'doses/new'
+  get 'doses/create'
+  get 'doses/destroy'
+  resources :cocktails, only: [:index, :show, :new, :create]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
